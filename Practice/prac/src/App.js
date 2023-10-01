973eba62f7d342540b238aef6d015ea3952f00e3
anshumancodes <anshumanprof01@gmail.com>
2023-10-01 23:20:41 +0530

    add item in cart component

--- Practice/prac/src/App.js
+++ Practice/prac/src/App.js
@@ -1,4 +1,26 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+function App() {
 
+  let item={
+    name:"Iphone",
+    price:100000,
+    color:"black"
+  }
+  return (
+    <div>
+      <h1>cart</h1>
+    <h2>items</h2>
+    <input type="range" id="yo"></input>
+    
+    
+    <p>name:{item.name}</p>
+    <p>price:{item.price}</p>
+    <p>color:{item.color}</p>
+    
+    </div>
+  )
+  }
+  
+  export default App;
