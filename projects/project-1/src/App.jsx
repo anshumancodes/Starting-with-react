f048f44cf9afc19e6644163eaa57771c19af50e5
anshumancodes <anshumanprof01@gmail.com>
2023-10-02 20:36:25 +0530

    intial commit for a landing page project

---
+++ projects/project-1/src/App.jsx
@@ -0,0 +1,16 @@
+function App(){
+
+    return(
+        <>
+        <div className="container">
+          <nav>
+            <span className="logo">
+              <img src="/images/brand_logo.png" alt="" />
+            </span>
+          </nav>
+        </div>
+        </>
+    )
+}
+
+export default App
