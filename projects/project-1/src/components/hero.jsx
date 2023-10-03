75cbca4dcd03fa0f8bb6d2143271ae888f24f984
anshumancodes <anshumanprof01@gmail.com>
2023-10-03 07:28:56 +0530

    added buttons in the landing page - modifying hero components , compeleted the project

--- projects/project-1/src/components/hero.jsx
+++ projects/project-1/src/components/hero.jsx
@@ -1,29 +1,33 @@
 function hero() {
-
-    return (
-
-        <>
-        <div className="hero-xt">
+  return (
+    <>
+      <div className="hero-xt">
         <div className="hero-txt">
-        <h1 className="Herotxt">YOUR FEET 
-        DESERVE
-        THE BEST</h1>
-        <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+          <h1 className="Herotxt">YOUR FEET DESERVE THE BEST</h1>
+          <p>
+            YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR
+            SHOES.
+          </p>
+
+          <div className="btn-container">
+            
+            <a href="">
+              <button className="hero-btn">Shop Now</button>
+            </a>
+            <a href="">
+              <button className="explore-btn">Shop Now</button>
+            </a>
+          </div>
         </div>
-        
 
         <div className="hero-imgx">
-        <img src="/images/shoe_image.png" alt="hero" />
+          <img src="/images/shoe_image.png" alt="hero" />
         </div>
 
-        </div>
-
-
-        
-        </>
-        
-    )
-    
+      </div>
+    </>
+  );
 }
 
-export default hero
+export default hero;
