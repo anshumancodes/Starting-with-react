1a2e65cf87a247810c5978c03799594a5f797b43
anshumancodes <anshumanprof01@gmail.com>
2023-10-08 23:36:53 +0530

    modifed buttonContainers element

--- projects/project-3/src/components/pages/Play.jsx
+++ projects/project-3/src/components/pages/Play.jsx
@@ -1,61 +1,50 @@
-import React from 'react'
-import styles from './Play.module.css'
+import React from "react";
+import styles from "./Play.module.css";
 
-const Play = () => {
+const Play = ({ text }) => {
+  const diceValue = [1, 2, 3, 4, 5, 6];
+
+  const diceNumber = Math.floor(Math.random() * diceValue.length);
+
   return (
     <div className={styles.Container}>
       <nav className={styles.Navbar}>
         <span className={styles.score}>
-
           <span className={styles.scorecounter}>
-            
             <span className={styles.scorevalue}>0</span>
             <span className={styles.scoretext}>Score</span>
-
           </span>
-
-
         </span>
 
         <span className={styles.Dicebuttons}>
+          <span className={styles.CheckText} text={text}></span>
 
-          <button className={styles.Dicebutton}>1</button>
-          <button className={styles.Dicebutton}>2</button>
-          <button className={styles.Dicebutton}>3</button>
-          <button className={styles.Dicebutton}>4</button>
-          <button className={styles.Dicebutton}>5</button>
-          <button className={styles.Dicebutton}>6</button>
-          
+          <div className={styles.buttonContainers}>
+
+            {diceValue.map((value, index) => (
+              <button className={styles.Dicebutton} key={index}>
+                {value}
+              </button>
+            ))}
+            
+          </div>
         </span>
-
-
       </nav>
 
       <div className={styles.Dicecontainer}>
+        <div className={styles.Dice}>
+          <img src={`/images/dice_${diceNumber}.png`} alt="" />
+        </div>
 
-      <div className={styles.Dice}> 
-      <img src="/images/dice_1.png" alt="" />
-      
-      </div>
-      
-
-      <div className={styles.ActionButtons}> 
-        
+        <div className={styles.ActionButtons}>
           <button className={styles.Actionbutton}>Roll Dice</button>
-          <button className={`${styles.Actionbutton} ${styles.BlackButton}`}>Show Rules</button>
-      
+          <button className={`${styles.Actionbutton} ${styles.BlackButton}`}>
+            Show Rules
+          </button>
+        </div>
       </div>
-
-
-        
-
-
-
-      </div>
-
-      
     </div>
-  )
-}
+  );
+};
 
-export default Play
+export default Play;
