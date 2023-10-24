6feb5971d313d758ea382ceed3103d3eb59dcfeb
anshumancodes <anshumanprof01@gmail.com>
2023-10-24 23:24:36 +0530

    Update Tailwind config with Discord-themed colors and fix purge settings

--- projects/discord-nav-tailwind/discord-nav/tailwind.config.js
+++ projects/discord-nav-tailwind/discord-nav/tailwind.config.js
@@ -5,7 +5,16 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      backgroundColor: {
+        'discord-primary': '#36393F',
+        'discord-sec': '#313338',
+        'discord-line':'linear-gradient(#36393f, #2f3136)',
+      },
+      backgroundImage: {
+        'discord-line': 'linear-gradient(#36393f, #2f3136)',
+      }
+    },
   },
   plugins: [],
 }
