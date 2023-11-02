acdf7d83cbc8349399b27964ff78cabf6df67138
anshumancodes <anshumanprof01@gmail.com>
2023-11-03 02:16:10 +0530

    updated app.jsx complted the action and branding panel

--- projects/project-6/src/App.jsx
+++ projects/project-6/src/App.jsx
@@ -1,14 +1,24 @@
-import { useState } from 'react'
-
+import React from "react";
 
 function App() {
-  
-  
   return (
-    <>
-    
-    </>
-  )
+    <div className="flex items-center flex-col h-screen">
+
+      <div className="bg-white p-6 rounded-full flex justify-center items-center text-black gap-1vw w-90% mt-5 h-5 gap-x-1">
+        <img src="imgs/logos_firebase.svg" alt="Firebase Logo" className="bg-white rounded-full" />
+        <h1 className="bg-white font-semibold text-xl">Firebase Contact App</h1>
+      </div>
+
+
+      <div className="flex gap-1vw w-90% mt-4">
+        <input type="search" className="outline-none border border-white h-40px rounded-10px text-white w-80%" placeholder="  Search" />
+      </div>
+
+      
+    </div>
+  );
 }
 
-export default App
+export default App;
+
+
